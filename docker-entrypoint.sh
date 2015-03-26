ceaa525cc040623be314e86bbd2d583439cd7cab
Anthony Stanton <anthony@contentful.com>
2015-03-26 15:07:28 +0100

    Fetch config from S3

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,9 +4,12 @@ set -ex
 if [ "$1" = 'nginx' ]; then
 
     if [ ! -z "$S3_BUCKET" ]; then
+        if [ ! -z "$S3_CONFIG" ]; then
+            /usr/bin/s3cmd -f get s3://$S3_BUCKET/$S3_CONFIG /etc/nginx/conf.d/default.conf
+        fi
     
         if [ ! -z "$S3_HTPASSWD" ]; then
-            /usr/bin/s3cmd get s3://$S3_BUCKET/$S3_HTPASSWD /etc/nginx/htpasswd        
+            /usr/bin/s3cmd -f get s3://$S3_BUCKET/$S3_HTPASSWD /etc/nginx/htpasswd        
         fi
     fi
 
